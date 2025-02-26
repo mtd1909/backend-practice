ed93c2c40dae85d84bb8bc2583f47a3d4eefa099
datmt <dat.maithanh19@gmail.com>
2025-02-26 21:23:47 +0700

    fix

--- database.js
+++ database.js
@@ -1,12 +1,40 @@
-const mysql = require("mysql2")
-const port = process.env.PORT || 8080
-const hostname = process.env.HOST_NAME
-require('dotenv').config()
-const connection = mysql.createConnection({
-  host: 'localhost',
-  database: 'test_database',
-  user: 'root',
-  password: 'Mtd19092001@'
-})
+require('dotenv').config();
+// const mysql = require("mysql2")
+// const port = process.env.PORT || 8080
+// const hostname = process.env.HOST_NAME
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   database: 'test_database',
+//   user: 'root',
+//   password: 'Mtd19092001@'
+// })
 
-module.exports = connection;
+// module.exports = connection;
+
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+
+let _db = null;
+
+async function connectToDatabase() {
+    if (!_db) {
+        const connectionString = process.env.DB_URL;
+        const dbName = process.env.DB_NAME;
+
+        const client = new MongoClient(connectionString); // Không cần truyền thêm options
+        await client.connect();
+        _db = client.db(dbName);
+    }
+    return _db;
+}
+
+async function ping() {
+    const db = await connectToDatabase();
+    await db.command({ ping: 1 });
+    console.log('Pinged the database');
+}
+
+ping();
+
+module.exports = connectToDatabase;
+module.exports.ping = ping;
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 const express = require('express')
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 const cors = require('cors')
 const app = express()
+// const connection = require('./database')
 const connection = require('./database')
+const connectToDatabase = require('./database')
+const ping = require('./database')
 const port = process.env.PORT || 8080
 require('dotenv').config()
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.get('/employees', function (req, res) {
-  const sql = "SELECT * FROM EMPLOYEES";
-  connection.query(sql, function (err, results) {
-    if (err) {
-      return res.status(500).json({ error: err })
-    }
-    return res.status(200).json({ data: results })
-  });
-});
+// app.get('/employees', function (req, res) {
+//   const sql = "SELECT * FROM EMPLOYEES";
+//   connection.query(sql, function (err, results) {
+//     if (err) {
+//       return res.status(500).json({ error: err })
+//     }
+//     return res.status(200).json({ data: results })
+//   });
+// });
+
+// async function getComments(req, res) {
+//   try {
+//       const db = await connectToDatabase();
+//       const comments = await db.collection("sales").find().toArray();
+//       res.json(comments);
+//   } catch (error) {
+//       res.status(500).json({ error: error.message });
+//   }
+// }
+
+// app.get("/comments", getComments);
 
 app.listen(port, function() {
   console.log(`App listening on port ${port}`);
-  connection.connect(function(err) {
-    if(err) throw err;
-    console.log('Database connected')
-  })
+//   connection.connect(function(err) {
+//     if(err) throw err;
+//     console.log('Database connected')
+//   })
 })
+
