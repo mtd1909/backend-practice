03875da659df5f8960f2236600b07273b71145fa
datmt <dat.maithanh19@gmail.com>
2025-05-31 04:09:26 +0700

    fix bug

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "https://backend-practice-production-66bb.up.railway.app/auth/google/callback",
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
